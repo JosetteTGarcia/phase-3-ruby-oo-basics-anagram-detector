4107a8dbd78e4be48926a254c38e511bc4ce341d
Josette Garcia <josettetgarcia@gmail.com>
2022-05-14 12:17:24 -0500

    commit tree 01fe688828a710c81c5015c49ae8324523e116f5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+attr_reader :word, :match
+  
+
+def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_array)
+    matching_word = anagram_array.filter do |word_in_array|
+      @word.chars.sort === word_in_array.chars.sort
+    end
+    matching_word
+  end
+
+
+
+end
